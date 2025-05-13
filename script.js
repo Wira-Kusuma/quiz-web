a36e80b0024c73bbcd91d7833292f04172c61ad6
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-05-13 21:51:55 +0700

    add score and stage system

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 function cekJawaban(nilai) {
-    let score = document.getElementById("score");
+    let scoreElem = document.getElementById("score");
+    let stageElem = document.getElementById("stage");
     const benar = 15744755;
+    let currentScore = Number(scoreElem.innerText);
+    let currentStage = Number(stageElem.innerText);
+
     if (nilai === benar) {
         alert("Benar!");
-        score.innerText= + " +100";
+        currentScore += 100;
+        currentStage += 1;
     } else {
         alert("Salah!");
+        currentScore -= 100;
+        currentStage += 1;
     }
+
+    scoreElem.innerText = currentScore;
+    stageElem.innerText = currentStage;
 }
