1930028fd2f566bb2cb97c5f8ee2fb12b3de6f2c
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-05-13 14:08:36 +0700

    update

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-function jawaban1() {
-    alert("salah");
-}
-
-function jawaban2() {
-    alert ("benar"); alert.style.color="green";
+function cekJawaban(nilai) {
+    let score = document.getElementById("score");
+    const benar = 15744755;
+    if (nilai === benar) {
+        alert("Benar!");
+        score.innerHTML(100);
+    } else {
+        alert("Salah!");
+    }
 }
