623e101b67f3b98f80bc49b7caa04eed9221b0f7
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-05-13 13:41:42 +0700

    add css and js

---
+++ script.js
@@ -0,0 +1,10 @@
+function jawaban1() {
+    let pertama = document.getElementById("pertama");
+    let kedua = document.getElementById("kedua");
+    let ketiga = document.getElementById("ketiga");
+    let keempat = document.getElementById("keempat");
+
+    if (kedua) {
+        alert("benar");
+    }
+}
