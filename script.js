0f6a56a165b08abd67febd800939e9bf1b56f88d
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-05-14 06:44:41 +0700

    udpate

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function cekJawaban(nilai) {
     let scoreElem = document.getElementById("score");
     let stageElem = document.getElementById("stage");
+    let soal1 = document.getElementById("soal1");
+    let soal2 = document.getElementById("soal2");
     const benar = 15744755;
     let currentScore = Number(scoreElem.innerText);
     let currentStage = Number(stageElem.innerText);
@@ -9,6 +11,7 @@ function cekJawaban(nilai) {
         alert("Benar!");
         currentScore += 100;
         currentStage += 1;
+
     } 
 
     else {
